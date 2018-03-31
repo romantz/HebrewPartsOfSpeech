07aaea2823c6e6d7fda569f882e4be034862d73b
romantzr <romantzr@gmail.com>
2018-03-31 19:01:42 +0300

    Added train logic

--- src/train.py
+++ src/train.py
@@ -2,9 +2,33 @@ import utils
 import sys  # This library is imported in order to access execution arguments
 
 if len(sys.argv) != 4:
-    print "Correct calling format is ./train < model > < heb-pos.train > < smoothing(y/n) >"
+    print 'Incorrect number of arguments'
+    print 'Correct calling format is: ./train < model > < heb-pos.train > < smoothing(y/n) >'
     exit(0)
 
 model = sys.argv[1]
 fileName = sys.argv[2]
 smoothing = sys.argv[3]
+
+if smoothing == 'y':
+    smoothing = True
+elif smoothing == 'n':
+    smoothing = False
+else:
+    print 'Incorrect calling format'
+    print 'Correct calling format is: ./train < model > < heb-pos.train > < smoothing(y/n) >'
+    exit(0)
+
+trainSegmentTagsDict, _, _, _ = utils.analyzeFile(fileName)
+
+f = open('../exps/param.lex','w')
+
+if int(model) == 1:
+    for segment, tagsDict in trainSegmentTagsDict.iteritems():
+        row = segment
+        totalSegmentOccurrences = sum(tagsDict.itervalues())
+        for tag, count in tagsDict.iteritems():
+            row += '\t' + tag + '\t' + str(count / float(totalSegmentOccurrences))
+        f.write(row)
+f.close()
+        
